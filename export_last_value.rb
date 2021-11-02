9a6f2e799cdfd3a020c8312507ea37151df88e69
moonstruckdrops <moonstruckdrops@gmail.com>
2021-11-02 22:51:01 +0900

    add label

--- export_last_value.rb
+++ export_last_value.rb
@@ -17,14 +17,14 @@ sidekiq_processed_view = OpenCensus::Stats::View.new(
   measure: sidekiq_processed_measure,
   aggregation: OpenCensus::Stats.create_last_value_aggregation,
 #  description: "sidekiq job processed count.",
-  columns: ["queue"] #=> tagsのキーと紐づく
+  columns: ["queue", "service"] #=> tagsのキーと紐づく
 )
 
 # ここで時系列データになる値を設定
 # このとき、tagsのキーがviewとmeasureデータを紐付ける役割を担う
 # tagsのvalueは対象の項目名(ターゲットと読み替えてもいいかも)
-measurement = sidekiq_processed_view.measure.create_measurement(value: 74556639, tags: { "queue" => "default" })
-measurement_worker = sidekiq_processed_view.measure.create_measurement(value: 74566639, tags: { "queue" => "test_worker" })
+measurement = sidekiq_processed_view.measure.create_measurement(value: 74556639, tags: { "queue" => "default", "service"=>"metrics.pupupu.cafe" })
+measurement_worker = sidekiq_processed_view.measure.create_measurement(value: 74566639, tags: { "queue" => "test_worker", "service"=>"metrics.pupupu.cafe" })
 
 # 時系列データの紐付けを実施する
 # 複数の登録を行うこともできる
@@ -38,11 +38,12 @@ sidekiq_processed_view_data.record(measurement_worker)
 # 2つめの引数は、対応するリソースタイプを設定するが、以下から決定する必要がある(該当するものがなければglobal)
 # https://cloud.google.com/monitoring/api/resources
 # 3つめの引数は、以下にあたるものっぽいが、最初の設定時点でデータがなければ実は登録できない(なんならAPI実行でエラーになる)
+# この内容はReadOnlyっぽいので、すでに決まっている内容を使用するという形になる
 # https://cloud.google.com/monitoring/api/ref_v3/rpc/google.api#google.api.MonitoredResource
 # 4つめの引数は、変換対象のOpenCensusのViewData
 converter = OpenCensus::Stats::Exporters::Stackdriver::Converter.new("sidekiq")
 time_series = converter.convert_time_series "custom.googleapis.com", "global", { }, sidekiq_processed_view_data
-
+p time_series
 # API Clientは不変でよい
 client = Google::Cloud::Monitoring::V3::MetricService::Client.new do |config|
   config.lib_name = "opencensus"
